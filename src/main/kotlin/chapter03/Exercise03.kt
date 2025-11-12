4d9ad3c5d684ed50d0ba0da2737ef25f6beeb413
OHJOUYEON <hyperclutter@gmail.com>
2025-11-12 19:01:56 +0900

    feat: 3장 학습

---
+++ src/main/kotlin/chapter03/Exercise03.kt
@@ -0,0 +1,22 @@
+package org.example.chapter03
+
+fun exercise03() {
+    val a = 10
+    var b = "ABC"
+    println(a)
+    println(b)
+
+    b = "CDE"
+    println(b)
+}
+
+fun mexercise03_02() {
+    // 변수 이름 다음에 콜론과 타입을 붙여 특정 타입을 명시할 수 있음
+    val a: Int = 10
+    var b: String = "ABC"
+    println(a)
+    println(b)
+    b = "CDE"
+    println(b)
+}
+
